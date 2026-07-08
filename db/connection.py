0f080c31863faec3716a7fa954da18cceace67a3
RuchitaGanurkar <ruchita.ganurkar@g10x.com>
2026-07-08 11:28:48 +0530

    SSL Added In DB String

--- db/connection.py
+++ db/connection.py
@@ -22,7 +22,7 @@ def get_engine():
 
     connection_string = (
         f"postgresql+psycopg2://{db_user}:{db_password}"
-        f"@{db_host}:{db_port}/{db_name}"
+        f"@{db_host}:{db_port}/{db_name}?sslmode=require"
     )
 
     return create_engine(connection_string)
